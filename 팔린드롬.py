0254b5110056bae84a650fe31a12ffd940763a2e
JuicyJerry <ju2cyjerry@gmail.com>
2020-03-18 22:55:42 +0900

    알고리즘의 정석, ch1 좋은 알고리즘이란?, 실습 예제
    팔린드룸

---
+++ 팔린드롬.py
@@ -0,0 +1,23 @@
+# "토마토"나 "기러기"처럼 거꾸로 읽어도 똑같은 단어를 팔린드롬(palindrome)이라고 부릅니다.
+# 문자열 word가 팔린드롬인지 확인하는 함수 is_palindrome를 쓰세요. is_palindrome은 word가 팔린드롬이면 True를,
+# 팔린드롬이 아니면 False를 리턴합니다.
+
+def is_palindrome(word):
+    list_word = list(word)
+
+    count = list_word[]
+    for i in list_word:
+        if i
+
+
+# 테스트
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
+
+
+        #if list_word[count] == list_word[count]:
+         #   print(True)
+        # another_count 가 안 먹혀?
